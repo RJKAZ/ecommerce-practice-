2aeae283e665f903883be125199200264d6c621a
Richard Kazanowski <rjkaz@comcast.net>
2021-03-26 18:37:51 -0400

    product listing fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
 
+
 function App() {
   return (
     <>
     <Header />
     <main className='py-3'>
       <Container>
-      <HomeScreen />
+        <HomeScreen />
       </Container>
     
     </main>
